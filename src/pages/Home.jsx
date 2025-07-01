770fc13593c936f2726d36974fdd630b07ab210a
KorawichInt <korawich.inthamusik@gmail.com>
2025-07-01 15:39:14 +0700

    add Home and Favorites pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "Spider-Man: Homecoming", release_date: "2017" },
+    { id: 2, title: "Spider-Man: Far From Home", release_date: "2019" },
+    { id: 3, title: "Spider-Man: No Way Home", release_date: "2022" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for a movie"
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button"></button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
